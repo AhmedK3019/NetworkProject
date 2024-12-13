954ccb6bd9852926c27218535182057bb2f76521
Ahmed Khalid <ahmedkhalid958666@gmail.com>
2024-12-13 16:18:31 +0200

    added MongoDB connection

--- MyApp/app.js
+++ MyApp/app.js
@@ -3,6 +3,15 @@ var path = require('path');
 
 var app = express();
 
+var { MongoClient } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017';
+var client = new MongoClient(url);
+client.connect();
+var db = client.db('myDB');
+//insertion test
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
